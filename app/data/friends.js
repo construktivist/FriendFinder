4b9d018c8aebb3fa5e07e86e52aa778a9c20edcf
Keegan Kelly <keegankelly1@gmail.com>
2016-11-01 18:07:45 -0500

    Created api route to retreive data

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+
+var friendsArray = [
+	{
+	  name:"John",
+	  photo:"http://www.freelanceme.net/Images/default%20profile%20picture.png",
+	  scores:[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+		]     
+	}
+];
+
+module.exports = friendsArray;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,7 @@
+var friendsArray = require("../data/friends.js");
+
+module.exports = function (app){
+	app.get("/api/friends", function(request, response){
+		response.json(friendsArray);
+	});
+};
