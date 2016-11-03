2fbc81184e69326c85ac738d0086f944cb98d93f
Keegan Kelly <keegankelly1@gmail.com>
2016-11-02 22:51:38 -0500

    Implemented POST from DOM to Server

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,4 +4,8 @@ module.exports = function (app){
 	app.get("/api/friends", function(request, response){
 		response.json(friendsArray);
 	});
+
+	app.post("/api/friends", function(request, response){
+		friendsArray.push(request.body);
+	})
 };
